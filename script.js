937ff53051647fbe552533db1269b3440e780d56
Abhijith Gaganan <theab10hd@gmail.com>
2024-08-28 02:08:25 +0530

    Responsive design - home

---
+++ script.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("mobile-menu-button")
+  .addEventListener("click", function () {
+    var menu = document.getElementById("mobile-menu");
+    menu.classList.toggle("hidden");
+  });
