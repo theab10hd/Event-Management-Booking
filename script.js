eaecee5b80784b3a007fd4ab65e66843d7d5c192
Abhijith Gaganan <theab10hd@gmail.com>
2024-08-30 12:46:30 +0530

    Navbar Animation Done, FaQ Added, Design Alter.

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-document
-  .getElementById("mobile-menu-button")
-  .addEventListener("click", function () {
-    var menu = document.getElementById("mobile-menu");
-    menu.classList.toggle("hidden");
-  });
+function navbarTap() {
+  var menu = document.getElementById("mobile-menu");
+
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+    setTimeout(function () {
+      menu.classList.remove("-translate-y-full");
+      menu.classList.add("translate-y-0");
+    }, 10);
+  } else {
+    menu.classList.add("-translate-y-full");
+    menu.classList.remove("translate-y-0");
+    menu.addEventListener(
+      "transitionend",
+      function () {
+        menu.classList.add("hidden");
+      },
+      { once: true }
+    );
+  }
+}
+
+function toggleAccordion(id) {
+  const content = document.getElementById(id);
+  const icon = document.getElementById("icon-" + id);
+
+  if (content.classList.contains("hidden")) {
+    content.classList.remove("hidden");
+    icon.classList.add("rotate-180");
+  } else {
+    content.classList.add("hidden");
+    icon.classList.remove("rotate-180");
+  }
+}
